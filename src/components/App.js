3be9b24ef22048046397b6dcc25e65db57568169
Kento75 <kento2github@gmail.com>
2019-11-09 12:42:30 +0900

    Added sagas

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-      test
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        test
+      </div>
+    );
+  }
 }
 
 export default App;

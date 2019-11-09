6f88dc62ce80f8ffbb66cf7f870fb80dc3be2ffa
Kento75 <kento2github@gmail.com>
2019-11-09 18:47:15 +0900

    Added NewUserForm and UsersList UI

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ListGroup, ListGroupItem } from "reactstrap";
+import { ListGroup, ListGroupItem, Button } from "reactstrap";
 
 const UsersList = ({ users }) => {
     return (
@@ -18,7 +18,18 @@ const UsersList = ({ users }) => {
                 }
             }).map((user) => {
                 return (
-                    <ListGroupItem key={user.id}>{ user.firstName }</ListGroupItem>
+                    <ListGroupItem key={user.id}>
+                        <section style={{ display: "flex" }}>
+                            <div style={{ flexGrow: 1, margin: "auto 0" }}>
+                                { user.firstName } { user.lastName }
+                            </div>
+                            <div>
+                                <Button outline color="danger">
+                                    Delete
+                                </Button>
+                            </div>
+                        </section>
+                    </ListGroupItem>
                 );
             })}
         </ListGroup>

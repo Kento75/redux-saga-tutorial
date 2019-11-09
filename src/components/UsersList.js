cb71889f5320c2a3e56441c1007b726cdb52d889
Kento75 <kento2github@gmail.com>
2019-11-09 18:31:22 +0900

    Fixed data sort logic, firstName and lastName

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -4,7 +4,19 @@ import { ListGroup, ListGroupItem } from "reactstrap";
 const UsersList = ({ users }) => {
     return (
         <ListGroup>
-            { users.map((user) => {
+            { users.sort((a, b) => {
+                if(a.firstName > b.firstName) {
+                    return 1;
+                } else if(a.firstName < b.firstName) {
+                    return -1;
+                } else if(a.lastName > b.lastName) {
+                    return 1;
+                } else if(a.lastName < b.lastName) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }).map((user) => {
                 return (
                     <ListGroupItem key={user.id}>{ user.firstName }</ListGroupItem>
                 );
